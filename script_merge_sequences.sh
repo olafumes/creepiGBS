841ac221f5bd26d39581fefbc2fe10f492341514
olafumes <werner@um.es>
2020-01-09 14:41:40 +0100

    Create script_merge_sequences.sh

---
+++ script_merge_sequences.sh
@@ -0,0 +1,8 @@
+./merge_sequences -f AlDA1_joinedq -1 CTACA -5 TACAA
+./merge_sequences -f AlDA2_joinedq -1 CTACA -5 TACAA
+./merge_sequences -f AlDB1_joinedq -1 CTACA -5 TACAA
+./merge_sequences -f AlDB2_joinedq -1 CTACA -5 TACAA
+./merge_sequences -f AlPA1_joinedq -1 CTACA -5 TACAA
+./merge_sequences -f AlPA2_joinedq -1 CTACA -5 TACAA
+./merge_sequences -f AlPB1_joinedq -1 CTACA -5 TACAA
+./merge_sequences -f AlPB2_joinedq -1 CTACA -5 TACAA
