1001963decbaf55599f961cd97a3f87a9fd397c5
olafumes <werner@um.es>
2020-01-09 14:38:33 +0100

    Create script4.sh

---
+++ script4.sh
@@ -0,0 +1,8 @@
+ustacks -f ./AlDA1_joinedq.fq -p 2 -M 4 -i 1 -o ./stacks
+ustacks -f ./AlDA2_joinedq.fq -p 2 -M 4 -i 2 -o ./stacks
+ustacks -f ./AlDB1_joinedq.fq -p 2 -M 4 -i 3 -o ./stacks
+ustacks -f ./AlDB2_joinedq.fq -p 2 -M 4 -i 4 -o ./stacks
+ustacks -f ./AlPA1_joinedq.fq -p 2 -M 4 -i 5 -o ./stacks
+ustacks -f ./AlPA2_joinedq.fq -p 2 -M 4 -i 6 -o ./stacks
+ustacks -f ./AlPB1_joinedq.fq -p 2 -M 4 -i 7 -o ./stacks
+ustacks -f ./AlPB2_joinedq.fq -p 2 -M 4 -i 8 -o ./stacks
