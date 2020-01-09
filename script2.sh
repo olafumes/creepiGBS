5dae0c0f7c383d1ca276805bfff6934cefb25fea
olafumes <werner@um.es>
2020-01-09 14:36:57 +0100

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,8 @@
+usearch10 -fastq_join AlDA1_R1_short.fq -reverse AlDA1_R2_short.fq -join_padgap ATATATAT -join_padgapq IIIIIIII -fastqout ./joined/AlDA1_joined.fq
+usearch10 -fastq_join AlDA2_R1_short.fq -reverse AlDA2_R2_short.fq -join_padgap ATATATAT -join_padgapq IIIIIIII -fastqout ./joined/AlDA2_joined.fq
+usearch10 -fastq_join AlDB1_R1_short.fq -reverse AlDB1_R2_short.fq -join_padgap ATATATAT -join_padgapq IIIIIIII -fastqout ./joined/AlDB1_joined.fq
+usearch10 -fastq_join AlDB2_R1_short.fq -reverse AlDB2_R2_short.fq -join_padgap ATATATAT -join_padgapq IIIIIIII -fastqout ./joined/AlDB2_joined.fq
+usearch10 -fastq_join AlPA1_R1_short.fq -reverse AlPA1_R2_short.fq -join_padgap ATATATAT -join_padgapq IIIIIIII -fastqout ./joined/AlPA1_joined.fq
+usearch10 -fastq_join AlPA2_R1_short.fq -reverse AlPA2_R2_short.fq -join_padgap ATATATAT -join_padgapq IIIIIIII -fastqout ./joined/AlPA2_joined.fq
+usearch10 -fastq_join AlPB1_R1_short.fq -reverse AlPB1_R2_short.fq -join_padgap ATATATAT -join_padgapq IIIIIIII -fastqout ./joined/AlPB1_joined.fq
+usearch10 -fastq_join AlPB2_R1_short.fq -reverse AlPB2_R2_short.fq -join_padgap ATATATAT -join_padgapq IIIIIIII -fastqout ./joined/AlPB2_joined.fq
