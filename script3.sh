f3487967cf75c1e59792e04ea87d8ff3caf95ee3
olafumes <werner@um.es>
2020-01-09 14:37:36 +0100

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,8 @@
+usearch10 -fastq_filter AlDA1_joined.fq -fastq_maxee_rate 0.01 -fastqout ./quality/AlDA1_joinedq.fq
+usearch10 -fastq_filter AlDA2_joined.fq -fastq_maxee_rate 0.01 -fastqout ./quality/AlDA2_joinedq.fq
+usearch10 -fastq_filter AlDB1_joined.fq -fastq_maxee_rate 0.01 -fastqout ./quality/AlDB1_joinedq.fq
+usearch10 -fastq_filter AlDB2_joined.fq -fastq_maxee_rate 0.01 -fastqout ./quality/AlDB2_joinedq.fq
+usearch10 -fastq_filter AlPA1_joined.fq -fastq_maxee_rate 0.01 -fastqout ./quality/AlPA1_joinedq.fq
+usearch10 -fastq_filter AlPA2_joined.fq -fastq_maxee_rate 0.01 -fastqout ./quality/AlPA2_joinedq.fq
+usearch10 -fastq_filter AlPB1_joined.fq -fastq_maxee_rate 0.01 -fastqout ./quality/AlPB1_joinedq.fq
+usearch10 -fastq_filter AlPB2_joined.fq -fastq_maxee_rate 0.01 -fastqout ./quality/AlPB2_joinedq.fq
